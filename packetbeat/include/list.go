9b2e14cd43dfacc00fea7e741e2d21f589be84a5
Grant Curell <grant.curell@salientcrgt.com>
2018-12-13 18:58:59 -0600

    updates

--- packetbeat/include/list.go
+++ packetbeat/include/list.go
@@ -38,6 +38,7 @@ import (
 	_ "github.com/elastic/beats/packetbeat/protos/nfs"
 	_ "github.com/elastic/beats/packetbeat/protos/pgsql"
 	_ "github.com/elastic/beats/packetbeat/protos/redis"
+	_ "github.com/elastic/beats/packetbeat/protos/ssh"
 	_ "github.com/elastic/beats/packetbeat/protos/tcp"
 	_ "github.com/elastic/beats/packetbeat/protos/thrift"
 	_ "github.com/elastic/beats/packetbeat/protos/tls"
--- packetbeat/protos/ssh/parser.go
+++ packetbeat/protos/ssh/parser.go
@@ -1,3 +1,22 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// This file is used to parse the inbound data received by packetbeat
+
 package ssh
 
 import (
@@ -91,6 +110,7 @@ func (p *parser) feed(ts time.Time, data []byte) error {
 	return nil
 }
 
+// Your protocol will begin parsing here. This is where you should start
 func (p *parser) newMessage(ts time.Time) *message {
 	return &message{
 		Message: applayer.Message{
